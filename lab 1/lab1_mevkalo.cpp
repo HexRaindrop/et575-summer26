c0702ab2f2c750ccd6db4891bd4bc0f9b803a886
HexRaindrop <jrmikey2006@gmail.com>
2026-05-28 15:30:11 +0000

    modified:   lab 1/lab1_mevkalo 	modified:   lab 1/lab1_mevkalo.cpp

--- lab 1/lab1_mevkalo.cpp
+++ lab 1/lab1_mevkalo.cpp
@@ -5,9 +5,34 @@ lab 1: iostream
 */
 
 #include <iostream>
+#include <string>
+
 
 using namespace std;
 int main(){
-    cout << "hello world!";
+    //varibles 
+    string country;
+    string gender;
+
+    // input
+    cout << "please enter a countery of origin\t\t:";
+    cin >> country;
+    cout<< "please enter a gender \n[m for male, f for female, o for other]\t :";
+    cin >> gender;
+
+    // gender var converstion
+    if (gender == "m"){
+        gender = "male";
+    }
+    if (gender == "f"){
+        gender = "female";
+    }
+    if (gender == "o"){
+        gender = "other";
+    }
+
+    // output
+    cout << "Entered country:\t" << country << "\n";
+    cout << "entered gender :\t" << gender << "\n";
     return 0;
 }
