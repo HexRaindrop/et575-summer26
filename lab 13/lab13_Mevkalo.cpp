ec1e278b11644785fae4661338787b0e1d9b78a1
HexRaindrop <jrmikey2006@gmail.com>
2026-06-10 14:45:17 +0000

    start of lab 12 and 13

---
+++ lab 13/lab13_Mevkalo.cpp
@@ -0,0 +1,23 @@
+/*
+Mike Mevkalo
+june 10, 2026
+lab 13, functions
+*/
+
+#include<iostream>
+#include "lab13_functions_mevkalo.cpp"
+
+using namespace std;
+
+int main(){
+
+    cout << "---- example 1 ----" << endl;
+    printhi();
+
+    cout << "---- example 2 ----" << endl;
+    greeting("peter");
+    string user = "annie";
+    greeting(user);
+    return 0;
+}
+
---
+++ lab 13/lab13_functions_mevkalo.cpp
@@ -0,0 +1,18 @@
+/*
+Mike Mevkalo
+june 10, 2026
+lab 13, functions
+*/
+
+#include<iostream>
+
+using namespace std;
+
+void printhi(){
+    cout << "hello world!" << endl;
+    return;
+}
+
+void greeting (string username){
+    cout << "hello " << username << endl;
+}
