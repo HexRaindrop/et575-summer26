8da90459c1fca80a0fec83325f104cff83fb0536
HexRaindrop <jrmikey2006@gmail.com>
2026-06-12 02:45:19 +0000

    modified:   lab 13/lab13_Mevkalo.cpp 	modified:   lab 13/lab13_functions_mevkalo.cpp

--- lab 13/lab13_Mevkalo.cpp
+++ lab 13/lab13_Mevkalo.cpp
@@ -18,10 +18,25 @@ int main(){
     greeting("peter");
     string user = "annie";
     greeting(user);
-
     cout << "---- example 3 ----" << endl;
+    int x = dbnumber(6);
+    cout << x << endl;
+    
+    cout << "---- example 4 ----" << endl;
+    float a = arearectangle(2.5,2);
+    cout << a << endl;
+    
+    cout << "---- example 5 ----" << endl;
+    string r = checknumber(-2);
+    cout << r << endl;
+    
+    cout << "---- example 6 ----" << endl;
+    int side = positevnumber();
+    int asquare = areasquare(side);
+    printresult(asquare);
 
-    dbnumber(1);
+    cout << "---- example 7 ----" << endl;
+    cout << tripleaddtion() << endl;
     return 0;
 }
 
--- lab 13/lab13_functions_mevkalo.cpp
+++ lab 13/lab13_functions_mevkalo.cpp
@@ -20,3 +20,34 @@ void greeting (string username){
 int dbnumber(int n){
     return n*2;
 }
+float arearectangle(float width, float lenght) {
+    return width * lenght;
+}
+
+string checknumber(int number){
+    if (number > 0) return "positve";
+    else if (number < 0) return "negative";
+    else return "zero";
+    }
+int areasquare(int side){
+    return side * side;
+    }
+int positevnumber(){
+    int n;
+    cout << "enter a number: "; cin >> n;
+    if (n>=0) return n;
+    while(!(n>=0)){
+        cout << "error, try again: "; cin >> n;
+        if (n>=0) return n;
+        }
+    }
+int addtion(int a, int b){
+    return a+b;
+}
+void printresult(int area){
+    cout << "the area of a square is " << area << endl;
+    return;
+}
+int tripleaddtion(){
+    return 3*(addtion(2,3));
+}
