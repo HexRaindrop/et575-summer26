a0314a4abf9b8a35651eb44e9d27aa00db4b118d
HexRaindrop <jrmikey2006@gmail.com>
2026-06-12 19:38:45 +0000

    lab 12 compleatsion

--- lab 12/lab12_mevkalo.cpp
+++ lab 12/lab12_mevkalo.cpp
@@ -5,7 +5,7 @@ lab 12, nested loops
 */
 
 #include<iostream>
-
+#include<string>
 using namespace std;
 
 int main(){
@@ -49,6 +49,36 @@ int main(){
                 (row >=5 && row <=6 && col >=5 && col <=6) ||
                 (row >=3 && row <=8 && col >=7 && col <=8))
                 cout << " x ";
+            else cout << " . ";
+        }
+        cout << endl;
+    }
+
+    cout << "---- excersise A ----" << endl;
+    const int random_num = 5;
+    int user_guess;
+    do {
+        cout << "enter a guess [1 - 10] : "; cin >> user_guess;
+        if (!(user_guess >=1 && user_guess <= 10)) {
+            cout << string(29,'-') << "error" << string(29,'-') << "\n| the entered guess is not bettween 1 or 10. please try again |" << endl << string(63,'-')<< endl;}
+        else if (user_guess == random_num) {
+            cout << "you guessed the right number!";
+            break;
+        }
+        else if (user_guess > random_num) cout << "your guess is higher then the number";
+        else cout << "your guess is less then the number";
+        cout << "\ntry again! \n";
+    }
+    while(user_guess != random_num);
+
+    cout << "\n---- excersise B ----" << endl;
+    int gridsize = 10;
+    for (int row = 1; row <= gridsize ; row++){
+        for (int col = 1 ; col <= gridsize ; col++){
+            if  ((row >=3 && row <=8 && col >=3 && col <=4) ||
+                (row >=7 && row <=10 && col >=5 && col <=6) ||
+                (row >=3 && row <=8 && col >=7 && col <=8))
+                cout << " & ";
             else cout << " . ";
         }
         cout << endl;
