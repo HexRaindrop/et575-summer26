b86edc1d0e9c2687f6e67338ac38cfbc81e612eb
HexRaindrop <jrmikey2006@gmail.com>
2026-06-16 14:51:33 +0000

    new file:   lab16_mevkalo_functions.cpp

---
+++ lab16_mevkalo_functions.cpp
@@ -0,0 +1,22 @@
+/*
+Mike Mevkalo
+June 16, 2026
+Lab 16
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// example one 
+void intropinter(){
+int num = 12;
+char sym = '#';
+string n = "Peter";
+
+int* ptrint;
+char* ptrchar;
+string* ptrstring;
+
+cout<< ptrint<< endl;
+}
